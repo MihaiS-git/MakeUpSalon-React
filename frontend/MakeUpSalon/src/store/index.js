5a6a990208821c89ffcbeb71bcdb9ed447c2090e
MihaiS-git <ingsuciumihai@gmail.com>
2024-11-15 14:49:00 +0200

    remove the unused middleware

--- frontend/MakeUpSalon/src/store/index.js
+++ frontend/MakeUpSalon/src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import persistedReducer from './reducers'; // Import the persisted rootReducer
 import { persistStore } from 'redux-persist';
-/* import { tokenExpirationMiddleware } from './tokenExpirationMiddleware'; */
 
 // Configure the store with the persisted reducer
 const store = configureStore({
   reducer: persistedReducer,
-  /* middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tokenExpirationMiddleware) */
 });
 
 // Create a persistor instance for redux-persist
